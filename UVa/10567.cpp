f1b9c6724e2dcae953ae4c3749d963387899c694
abhisheksaran <abhishekbishnoi695@gmail.com>
2019-11-25 19:25:30 +0530

    Long chanllenge Nov19

---
+++ UVa/10567.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	string str;
+	vector<vector<int> vii (52,vector<int>());
+	cin>>str;
+	for(int i=0;i<n;i++){
+		
+	int q;
+	cin>>q;
+	for(int i=0;i<t;i++){
+		string qry;
+		cin>>qry;
+	
+			
+	}
+return 0;
+}
