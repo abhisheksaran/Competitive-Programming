1f38af96c630695845c1927a2f10bb2f7787e48d
abhisheksaran <abhishekbishnoi695@gmail.com>
2021-02-20 00:18:24 +0530

    codeforces round 703

---
+++ codeforces/c1486.cpp
@@ -0,0 +1,31 @@
+/* @author: saran
+ * Git: https://github.com/abhisheksaran
+ * LinkedIn: https://www.linkedin.com/in/abhishek-saran-2900b/
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+    	cin.tie(NULL);
+	int n;
+	cin>>n;
+	int s = 1;
+	int e = n;
+	while(s!=e){
+		int idx, idx1, idx2;
+		cout<<"? "<<l<<" "<<r<<"\n";
+		cin>>idx;
+		
+		
+		cout<<"? "<<l<<" "<<idx<<"\n";
+		cin>>idx1;
+		cout<<"? "<<idx<<" "<<r<<"\n";
+		cin>>idx2;
+
+		if(idx == idx1) s
+
+	}
+	return 0;
+}
